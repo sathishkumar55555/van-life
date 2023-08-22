5cbe8a6f9f4f6fcdd83237e5f991163b58beaaf3
sathishkumar55555 <sathishdon4672@gmail.com>
2023-08-22 15:45:32 +0530

    App Upload

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter ,Routes,Route,Link} from "react-router-dom"
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Home from './pages/Home';
+import About from './pages/About';
+
+
+function App(){
+  return (
+    <BrowserRouter>
+    <header>
+      <nav>
+      <Link to="/">#VanLife</Link>
+    <Link to="/about">About</Link>
+      </nav>
+    </header>
+
+    
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+    </Routes>
+  </BrowserRouter>
+  )
+}
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App/>
+  
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
